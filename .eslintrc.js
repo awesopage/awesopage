1ce4d6e67ee9ea39d8f5224bbc5e887efae9e592
Bao D <baocancode@gmail.com>
2022-10-05 21:41:20 +1100

    Use eslint to lint code

---
+++ .eslintrc.js
@@ -0,0 +1,71 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['file-progress', 'sonarjs', 'no-null', 'simple-import-sort'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:sonarjs/recommended',
+    'next/core-web-vitals',
+    'plugin:prettier/recommended',
+  ],
+  globals: {
+    globalThis: 'readonly',
+  },
+  rules: {
+    'file-progress/activate': 1,
+    '@typescript-eslint/no-type-alias': [
+      'error',
+      {
+        allowAliases: 'always',
+        allowCallbacks: 'always',
+        allowConditionalTypes: 'always',
+        allowMappedTypes: 'always',
+        allowTupleTypes: 'always',
+      },
+    ],
+    'max-lines-per-function': ['error', { max: 40, skipBlankLines: true, skipComments: true }],
+    'sonarjs/prefer-immediate-return': 'off',
+    'sonarjs/cognitive-complexity': ['error', 10],
+    'no-null/no-null': 'error',
+    'import/no-duplicates': 'error',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          // side effect imports
+          ['^\\u0000'],
+          // external imports
+          ['^'],
+          // internal imports
+          ['^ap-'],
+          // relative imports (should manually fix them to internal imports)
+          ['^\\.'],
+        ],
+      },
+    ],
+    'simple-import-sort/exports': 'error',
+    'react/display-name': 'off',
+  },
+  overrides: [
+    {
+      files: ['*.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+    {
+      files: ['*.tsx'],
+      rules: {
+        'max-lines-per-function': ['error', { max: 50, skipBlankLines: true, skipComments: true }],
+      },
+    },
+    {
+      files: ['*.test.ts'],
+      rules: {
+        'sonarjs/cognitive-complexity': 'off',
+        'sonarjs/no-duplicate-string': 'off',
+        'max-lines-per-function': 'off',
+      },
+    },
+  ],
+}
--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const workspacePackages = require('./workspace-packages.json')
+
 /**
  * @type {import('next').NextConfig}
  */
@@ -5,6 +7,17 @@ const nextConfig = {
   swcMinify: true,
   reactStrictMode: true,
   poweredByHeader: false,
+  eslint: {
+    dirs: [
+      ...workspacePackages
+        .map(({ name }) => name)
+        .filter((name) => name.startsWith('ap-'))
+        .map((name) => `packages/${name}/src`),
+      'pages',
+      '.eslintrc.js',
+      'next.config.js',
+    ],
+  },
 }
 
 module.exports = nextConfig
