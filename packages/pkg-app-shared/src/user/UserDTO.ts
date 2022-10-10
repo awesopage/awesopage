b1e851a03aba6e55728d2a0cd95dd8aa2c56e663
Bao D <baocancode@gmail.com>
2022-10-11 00:12:43 +1100

    Update user DTO

--- packages/pkg-app-shared/src/user/UserDTO.ts
+++ packages/pkg-app-shared/src/user/UserDTO.ts
@@ -2,4 +2,7 @@ export interface UserDTO {
   readonly id: string
   readonly email: string
   readonly displayName: string
+  readonly roles: string[]
+  readonly createdAt: string
+  readonly updatedAt: string
 }
