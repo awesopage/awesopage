8c18dffba4c99449dd487d36abbf3312ac4e82fe
Bao D <30614076+baocancode@users.noreply.github.com>
2022-10-11 00:43:34 +1100

    Set up app store (#30)

---
+++ packages/pkg-app-shared/src/user/UserDTO.ts
@@ -0,0 +1,8 @@
+export interface UserDTO {
+  readonly id: string
+  readonly email: string
+  readonly displayName: string
+  readonly roles: string[]
+  readonly createdAt: string
+  readonly updatedAt: string
+}
