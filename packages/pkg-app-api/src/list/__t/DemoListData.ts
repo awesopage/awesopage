34d655fdea59be3b7e5995e586730f3a15b1d91f
Bao D <baocancode@gmail.com>
2022-10-14 21:40:52 +1100

    Add options to create demo data

--- packages/pkg-app-api/src/list/__t/DemoListData.ts
+++ packages/pkg-app-api/src/list/__t/DemoListData.ts
@@ -3,17 +3,18 @@ import * as fake from 'fictional'
 import { ListDTO } from 'pkg-app-shared/src/list/ListDTO'
 import { getRange } from 'pkg-lib-common/src/CollectionUtils'
 
-export const getDemoLists = (): ListDTO[] => {
+export const createDemoLists = (size = 60, defaultProps: Partial<ListDTO> = {}): ListDTO[] => {
   const owners: string[] = getRange(1, 5).map((ownerIndex) => `owner${ownerIndex}`)
   const topics: string[] = getRange(1, 10).map((topicIndex) => `topic${topicIndex}`)
 
-  return getRange(1, 60).map((listIndex): ListDTO => {
+  return getRange(1, size).map((listIndex): ListDTO => {
     return fake.shape(listIndex, {
       id: `${listIndex}`,
       repoKey: fake.join('/', [fake.oneOf(owners), `repo${listIndex}`]),
       description: fake.sentence.options({ unicode: false }),
       starCount: fake.int.options({ min: 1_000, max: 10_000 }),
       tags: fake.someOf([2, 4], topics),
+      ...defaultProps,
     })
   })
 }
