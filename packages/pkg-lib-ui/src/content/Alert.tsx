fd9818b88364c8454b992c5943892286b4aa1740
Bao D <baocancode@gmail.com>
2022-10-14 21:45:15 +1100

    Use numeric separator

--- packages/pkg-lib-ui/src/content/Alert.tsx
+++ packages/pkg-lib-ui/src/content/Alert.tsx
@@ -23,7 +23,7 @@ export type ToastStatus = ChakraAlertStatus
 export const useToast = () => {
   const chakraToast = useChakraToast({
     position: 'bottom',
-    duration: 2000,
+    duration: 2_000,
   })
 
   const toast = (title: string, status: ToastStatus) => {
