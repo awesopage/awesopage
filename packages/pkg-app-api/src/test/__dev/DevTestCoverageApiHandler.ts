219ff1771690bf1eab7040724e8e7006d4074003
Bao D <baocancode@gmail.com>
2022-10-20 01:04:39 +1100

    Use globalThis

--- packages/pkg-app-api/src/test/__dev/DevTestCoverageApiHandler.ts
+++ packages/pkg-app-api/src/test/__dev/DevTestCoverageApiHandler.ts
@@ -6,6 +6,6 @@ import { createApiRouter } from 'pkg-app-api/src/router/ApiRouter'
 export const devTestCoverageApiHandler: NextApiHandler = createApiRouter()
   .get(async (req, res: NextApiResponse) => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    sendApiResponse(res, (global as any).__coverage__ ?? {})
+    sendApiResponse(res, (globalThis as any).__coverage__ ?? {})
   })
   .handler()
