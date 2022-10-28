5a5b522d4b60f8a0bd9f74d2a971f644142a3f05
Minh Chau <nmchau87@gmail.com>
2022-10-28 20:21:30 +0900

    Fix hard coding

--- packages/pkg-app-web/src/list/ListsPage.tsx
+++ packages/pkg-app-web/src/list/ListsPage.tsx
@@ -11,26 +11,27 @@ import { useColorModeValue } from 'pkg-lib-ui/src/hook/ThemeHooks'
 import { Box } from 'pkg-lib-ui/src/layout/Box'
 import { Grid } from 'pkg-lib-ui/src/layout/Grid'
 import { Stack } from 'pkg-lib-ui/src/layout/Stack'
+import { SPACE_MEDIUM, SPACE_SMALL } from 'pkg-lib-ui/src/theme/SpaceValues'
 
 export const ListsPage: NextPage<ListsPageProps> = ({ lists }) => {
   const bgColor = useColorModeValue('white', 'black')
   const titleColor = useColorModeValue('primary.600', 'primary.200')
   return (
     <DefaultPageLayout>
-      <Grid templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(3, 1fr)' }} gap={5}>
+      <Grid templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(3, 1fr)' }} gap={SPACE_MEDIUM}>
         {lists.map((list) => (
-          <Box key={list.id} backgroundColor={bgColor} boxShadow={'2xl'} padding={5}>
-            <Stack minHeight={'200px'}>
+          <Box key={list.id} backgroundColor={bgColor} boxShadow={'2xl'} padding={SPACE_MEDIUM}>
+            <Stack minHeight='200px'>
               <Text fontWeight={800} fontSize={'sm'}>
                 {list.starCount} &#9734;
               </Text>
               <Heading color={titleColor} fontSize={'2xl'}>
                 {list.repoKey}
               </Heading>
-              <Text minHeight={'50px'}> {list.description} </Text>
+              <Text minHeight='50px'> {list.description} </Text>
               <Text>
                 {list.tags.map((tag) => (
-                  <Tag key={tag} mr={2}>
+                  <Tag key={tag} marginRight={SPACE_SMALL}>
                     {tag}
                   </Tag>
                 ))}
