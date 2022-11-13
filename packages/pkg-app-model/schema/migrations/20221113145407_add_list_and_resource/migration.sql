6fabea0b5fb51ee8102d18fc42830f8156d0f674
Minh Chau <nmchau87@gmail.com>
2022-11-14 00:30:43 +0900

    Fix list and resource relation

--- packages/pkg-app-model/schema/migrations/20221113145407_add_list_and_resource/migration.sql
+++ packages/pkg-app-model/schema/migrations/20221113145407_add_list_and_resource/migration.sql
@@ -1,5 +1,5 @@
 -- CreateEnum
-CREATE TYPE "resource_type" AS ENUM ('CODE', 'VIDEO', 'DEFAULT');
+CREATE TYPE "resource_type" AS ENUM ('CODE', 'VIDEO', 'WEBSITE');
 
 -- CreateTable
 CREATE TABLE "list" (
@@ -12,7 +12,6 @@ CREATE TABLE "list" (
     "requested_by_id" INT8 NOT NULL,
     "updated_at" TIMESTAMP(3) NOT NULL,
     "approved_by_id" INT8,
-    "resource_id" INT8,
 
     CONSTRAINT "list_pkey" PRIMARY KEY ("id")
 );
@@ -25,12 +24,16 @@ CREATE TABLE "resource" (
     "type" "resource_type" NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL,
     "updated_at" TIMESTAMP(3) NOT NULL,
+    "list_id" INT8 NOT NULL,
 
     CONSTRAINT "resource_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "list_repo_key_key" ON "list"("repo_key");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "resource_url_key" ON "resource"("url");
 
 -- AddForeignKey
 ALTER TABLE "list" ADD CONSTRAINT "list_requested_by_id_fkey" FOREIGN KEY ("requested_by_id") REFERENCES "app_user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -39,4 +42,4 @@ ALTER TABLE "list" ADD CONSTRAINT "list_requested_by_id_fkey" FOREIGN KEY ("requ
 ALTER TABLE "list" ADD CONSTRAINT "list_approved_by_id_fkey" FOREIGN KEY ("approved_by_id") REFERENCES "app_user"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "list" ADD CONSTRAINT "list_resource_id_fkey" FOREIGN KEY ("resource_id") REFERENCES "resource"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "resource" ADD CONSTRAINT "resource_list_id_fkey" FOREIGN KEY ("list_id") REFERENCES "list"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
