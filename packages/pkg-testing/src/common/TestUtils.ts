b89ed05ba9753e1974704772a8a328d3090a1d58
Bao D <baocancode@gmail.com>
2022-11-19 09:28:18 +0000

    Set snapshot path template

--- packages/pkg-testing/src/common/TestUtils.ts
+++ packages/pkg-testing/src/common/TestUtils.ts
@@ -2,15 +2,4 @@ import { expect as baseExpect, test as baseTest } from 'playwright-test-coverage
 
 export const expect = baseExpect
 
-//  Based on https://github.com/microsoft/playwright/issues/11134#issuecomment-1044707761
-export const test = baseTest.extend<{ _autoSnapshotSuffix: void }>({
-  _autoSnapshotSuffix: [
-    // eslint-disable-next-line no-empty-pattern
-    async ({}, use, testInfo) => {
-      testInfo.snapshotSuffix = ''
-
-      await use()
-    },
-    { auto: true },
-  ],
-})
+export const test = baseTest
