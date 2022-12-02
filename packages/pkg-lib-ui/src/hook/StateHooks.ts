b2a8e9076d97e09548235b61137918fc4dbdfdbb
Bao D <30614076+baocancode@users.noreply.github.com>
2022-12-03 00:31:42 +1100

    Make signin link auto focus (#129)

--- packages/pkg-lib-ui/src/hook/StateHooks.ts
+++ packages/pkg-lib-ui/src/hook/StateHooks.ts
@@ -1,3 +1,6 @@
 import { useDisclosure as useChakraDisclosure } from '@chakra-ui/react'
 
 export const useDisclosure = useChakraDisclosure
+
+// eslint-disable-next-line no-null/no-null
+export const EMPTY_REF = null
