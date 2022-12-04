8ed31a6f89543fc8aeda2c76079aab516e59be37
Bao D <30614076+baocancode@users.noreply.github.com>
2022-12-04 12:07:03 +1100

    Use stack for lists page (#132)

--- packages/pkg-app-web/src/list/ListsPage.tsx
+++ packages/pkg-app-web/src/list/ListsPage.tsx
@@ -3,16 +3,16 @@ import { NextPage } from 'next'
 import { ListsPageProps } from 'pkg-app-shared/src/list/ListsPageProps'
 import { ListCard } from 'pkg-app-web/src/list/ListCard'
 import { DefaultPageLayout } from 'pkg-app-web/src/page/DefaultPageLayout'
-import { Grid } from 'pkg-lib-ui/src/layout/Grid'
+import { Stack } from 'pkg-lib-ui/src/layout/Stack'
 
 export const ListsPage: NextPage<ListsPageProps> = ({ lists }) => {
   return (
     <DefaultPageLayout>
-      <Grid templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)' }}>
+      <Stack>
         {lists.map((list) => (
           <ListCard key={list.id} list={list} />
         ))}
-      </Grid>
+      </Stack>
     </DefaultPageLayout>
   )
 }
