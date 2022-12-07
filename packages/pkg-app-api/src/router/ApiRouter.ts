fc3404fd878d59ecc306ddaa980cc954e36c25e4
Bao D <30614076+baocancode@users.noreply.github.com>
2022-12-08 01:52:35 +1100

    Check request with signed in user (#134)

--- packages/pkg-app-api/src/router/ApiRouter.ts
+++ packages/pkg-app-api/src/router/ApiRouter.ts
@@ -4,17 +4,20 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { createRouter, expressWrapper, NextHandler } from 'next-connect'
 
 import { sendApiError } from 'pkg-app-api/src/router/ApiResponseSender'
+import { User } from 'pkg-app-model/client'
 import { createLogger } from 'pkg-app-service/src/common/LoggingUtils'
+import { prismaClient } from 'pkg-app-service/src/common/PrismaClient'
+import { findUserByEmail } from 'pkg-app-service/src/user/UserService'
 import { assertDefined } from 'pkg-lib-common/src/AssertUtils'
 
 const logger = createLogger('ApiRouter')
 
-export const requireProfile = (profile: string) => {
+export const checkProfile = (profile: string) => {
   return async (req: NextApiRequest, res: NextApiResponse, next: NextHandler) => {
     const profiles = process.env.APP_PROFILES ? process.env.APP_PROFILES.split(',') : []
 
     if (!profiles.includes(profile)) {
-      logger.debug(`Profile ${profile} is required but not found in [${profiles.join(', ')}]`)
+      logger.debug(`Profile ${profile} is required but not found in [${profiles.join(', ')}] of current environment`)
 
       return sendApiError(res, 'INTERNAL_SERVER_ERROR')
     }
@@ -44,6 +47,37 @@ const enableCookieSession = () => {
   })
 
   return expressWrapper(sessionHandler)
+}
+
+export const checkSignedIn = () => {
+  return async (req: NextApiRequest, res: NextApiResponse, next: NextHandler) => {
+    const reqWithUser = req as NextApiRequest & { user?: User }
+    const { email } = reqWithUser.session
+
+    if (!email) {
+      logger.debug(`Session does not have email`)
+
+      return sendApiError(res, 'UNAUTHORIZED')
+    }
+
+    const user: User = await prismaClient.$transaction((dbClient) => {
+      return findUserByEmail(dbClient, email)
+    })
+
+    reqWithUser.user = user
+
+    return next()
+  }
+}
+
+export const requireUser = (req: NextApiRequest): User => {
+  const { user } = req as NextApiRequest & { user?: User }
+
+  if (!user) {
+    throw new Error('Request does not have user')
+  }
+
+  return user
 }
 
 export const createApiRouter = <
